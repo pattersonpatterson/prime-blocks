512732dcb7876113f846f69de82abac22a2ea75d
patterson patterson <craze305@gmail.com>
2014-11-15 16:11:34 -0500

    Initial file commit: First draft of comment outline for program

---
+++ prime_blocks.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 15 15:31:42 2014
+
+@author: derpson
+"""
+
+# imports
+
+############### Getting data and arranging in Data.Frame
+# Take in value and check that it's not such a huge number
+
+
+# Find prime factorization of number and store factors (not 1) in Data.Frame
+
+
+# Randomly generate int(x,y) positions for each factor (bound to 10, maybe 7)
+# Positions must be unique in either x or y
+
+
+# Generate width for each factor as a multiple of factor in a logarithmic scale
+# All heights = 0.9
+
+
+# Assign specific colors to each prime factor
+
+
+############### Display 
+# Insert data into ColumnDataSource
+
+
+# hold() to put all the boxes on one grid
+
+# Make a rect glyph for each factor
+
+
+# Add text to display the number for each box
+
+
+# turn off grid lines - bk.grid().grid_line_color = None
+
+# show the chart
+
